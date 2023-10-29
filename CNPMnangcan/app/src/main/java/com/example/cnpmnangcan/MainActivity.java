c397841f31b9c71051f4ddb7a5afcca7f77aaa8b
TrietAnh222 <bun254872@gmail.com>
2023-10-29 20:35:59 +0700

    test ver gradle ổn

--- CNPMnangcan/app/src/main/java/com/example/cnpmnangcan/MainActivity.java
+++ CNPMnangcan/app/src/main/java/com/example/cnpmnangcan/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.cnpmnangcan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import io.realm.Realm;
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+
+
+public class MainActivity extends AppCompatActivity {
+    String Appid = "data-nhbur";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Realm.init(this);
+        App app = new App(new AppConfiguration.Builder(Appid).build());
     }
 }
---
+++ CNPMnangcan/app/src/main/java/com/example/cnpmnangcan/SplashActivity.java
@@ -0,0 +1,6 @@
+package com.example.cnpmnangcan;
+
+import android.app.Activity;
+
+public class SplashActivity extends Activity {
+}
